ceda92a9733d12666d8f20cc3d26d11ea00802f5
aklitzke <andrewknpe@gmail.com>
2020-02-21 15:04:14 -0600

    only panic-ing when not a test

--- lib.rs
+++ lib.rs
@@ -122,6 +122,7 @@ impl slog::Drain for NoGlobalLoggerSet {
            _record: &Record,
            _values: &OwnedKVList)
         -> result::Result<Self::Ok, Self::Err> {
+            #[cfg(not(test))]
             panic!(
             "slog-scope: No logger set. Use `slog_scope::set_global_logger` or `slog_scope::scope`."
             )
